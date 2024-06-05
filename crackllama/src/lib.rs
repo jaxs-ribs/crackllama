bc8d8f39d83394e66c158a66d73ea923d2d09820
jaxs-ribs <luc.baracat@googlemail.com>
2024-06-05 14:45:19 +0200

    wip

--- crackllama/src/lib.rs
+++ crackllama/src/lib.rs
@@ -1,7 +1,10 @@
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
-use kinode_process_lib::{await_message, call_init, println, Address, ProcessId, Request, Response, http};
+use kinode_process_lib::{await_message, call_init, println, Address, ProcessId, Message, Request, Response, http, get_blob};
+
+mod structs;
+use structs::*;
 
 wit_bindgen::generate!({
     path: "wit",
@@ -9,6 +12,47 @@ wit_bindgen::generate!({
 });
 
 fn handle_message(our: &Address) -> anyhow::Result<()> {
+    let msg = await_message()?;
+    if msg.source().node != our.node {
+        return Err(anyhow::anyhow!("message from {:?} is not from our node", msg.source()));
+    }
+    if msg.source().process == "http_server:distro:sys" {
+        handle_http_messages(&our, &msg);
+    }
+    Ok(())
+}
+
+fn handle_http_messages(our: &Address, msg: &Message) -> anyhow::Result<()> {
+    match msg {
+        Message::Request { ref body, .. } => {
+            handle_http_request(our, body);
+        },
+        Message::Response { .. } => {
+
+        },
+    }
+
+    Ok(())
+}
+
+fn handle_http_request(our: &Address, body: &[u8]) -> anyhow::Result<()> {
+    let http_request = http::HttpServerRequest::from_bytes(body)?
+        .request()
+        .ok_or_else(|| anyhow::anyhow!("Failed to parse http request"))?;
+    let path = http_request.path()?;
+    let bytes = get_blob()
+        .ok_or_else(|| anyhow::anyhow!("Failed to get blob"))?
+        .bytes;
+
+    match path.as_str() {
+        "/prompt" => prompt(our, &bytes),
+        _ => Ok(()),
+    }
+}
+
+fn prompt(our: &Address, bytes: &[u8]) -> anyhow::Result<()> {
+    let prompt = serde_json::from_slice::<Prompt>(bytes)?;
+    println!("prompt: {:?}", prompt);
     Ok(())
 }
 
