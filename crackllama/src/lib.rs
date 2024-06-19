b15849a121441b93fe895385706283e66f4c3084
jaxs-ribs <luc.baracat@googlemail.com>
2024-06-19 12:19:18 +0200

    Use interface

--- crackllama/src/lib.rs
+++ crackllama/src/lib.rs
@@ -1,6 +1,7 @@
 use kinode_process_lib::{await_message, call_init, get_blob, http, println, Address, Message};
 use std::collections::HashMap;
 use std::time::{SystemTime, UNIX_EPOCH};
+use storage_interface::CurrentConversation;
 
 mod structs;
 use structs::*;
@@ -12,6 +13,44 @@ wit_bindgen::generate!({
     path: "wit",
     world: "process",
 });
+
+// TODO: These are helper functions
+pub fn is_new(current_conversation: &CurrentConversation) -> bool {
+    current_conversation.messages.len() == 2
+        && current_conversation.date_created.is_none()
+        && current_conversation.title.is_none()
+}
+
+pub fn clear(current_conversation: &mut CurrentConversation) {
+    current_conversation.title = None;
+    current_conversation.messages = vec![];
+    current_conversation.date_created = None;
+}
+
+fn update_conversation(
+    prompt: &str,
+    answer: &str,
+    current_conversation: &mut CurrentConversation,
+) -> anyhow::Result<()> {
+    current_conversation.messages.push(prompt.to_string());
+    current_conversation.messages.push(answer.to_string());
+
+    if is_new(current_conversation) {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as i64;
+
+        current_conversation.date_created = Some(now);
+
+        let summary_prompt = format!("Given the following conversation: {:?}, summarize the topic in 80 words or less. Only output the title, do not explain yourself.", current_conversation.messages);
+        let summary_answer = get_groq_answer(&summary_prompt, &Model::Llama38B.get_model_name())?;
+        current_conversation.title = Some(summary_answer);
+    }
+
+    Ok(())
+}
+// TODO: /endtodo
 
 fn handle_message(our: &Address, state: &mut State) -> anyhow::Result<()> {
     let msg = await_message()?;
@@ -107,7 +146,7 @@ fn list_models() -> anyhow::Result<()> {
 }
 
 fn new_conversation(current_conversation: &mut CurrentConversation) -> anyhow::Result<()> {
-    current_conversation.clear();
+    clear(current_conversation);
     http::send_response(
         http::StatusCode::OK,
         Some(HashMap::from([(
@@ -116,30 +155,6 @@ fn new_conversation(current_conversation: &mut CurrentConversation) -> anyhow::R
         )])),
         "success".to_string().as_bytes().to_vec(),
     );
-    Ok(())
-}
-
-fn update_conversation(
-    prompt: &str,
-    answer: &str,
-    current_conversation: &mut CurrentConversation,
-) -> anyhow::Result<()> {
-    current_conversation.messages.push(prompt.to_string());
-    current_conversation.messages.push(answer.to_string());
-
-    if current_conversation.is_new() {
-        let now = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs() as i64;
-
-        current_conversation.date_created = Some(now);
-
-        let summary_prompt = format!("Given the following conversation: {:?}, summarize the topic in 80 words or less. Only output the title, do not explain yourself.", current_conversation.messages);
-        let summary_answer = get_groq_answer(&summary_prompt, &Model::Llama38B.get_model_name())?;
-        current_conversation.title = Some(summary_answer);
-    }
-
     Ok(())
 }
 
