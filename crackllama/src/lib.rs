219ba260148e6333d42390a2bdd903f9f4aad197
jaxs-ribs <luc.baracat@googlemail.com>
2024-06-19 10:27:33 +0200

    wip

--- crackllama/src/lib.rs
+++ crackllama/src/lib.rs
@@ -1,5 +1,6 @@
-use kinode_process_lib::{await_message, call_init, println, Address, Message, http, get_blob};
+use kinode_process_lib::{await_message, call_init, get_blob, http, println, Address, Message};
 use std::collections::HashMap;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 mod structs;
 use structs::*;
@@ -12,10 +13,16 @@ wit_bindgen::generate!({
     world: "process",
 });
 
-fn handle_message(our: &Address, current_conversation: &mut CurrentConversation) -> anyhow::Result<()> {
+fn handle_message(
+    our: &Address,
+    current_conversation: &mut CurrentConversation,
+) -> anyhow::Result<()> {
     let msg = await_message()?;
     if msg.source().node != our.node {
-        return Err(anyhow::anyhow!("message from {:?} is not from our node", msg.source()));
+        return Err(anyhow::anyhow!(
+            "message from {:?} is not from our node",
+            msg.source()
+        ));
     }
     if msg.source().process == "http_server:distro:sys" {
         return handle_http_messages(&msg, current_conversation);
@@ -24,20 +31,24 @@ fn handle_message(our: &Address, current_conversation: &mut CurrentConversation)
     Ok(())
 }
 
-fn handle_http_messages(msg: &Message, current_conversation: &mut CurrentConversation) -> anyhow::Result<()> {
+fn handle_http_messages(
+    msg: &Message,
+    current_conversation: &mut CurrentConversation,
+) -> anyhow::Result<()> {
     match msg {
         Message::Request { ref body, .. } => {
             return handle_http_request(body, current_conversation);
-        },
-        Message::Response { .. } => {
-
-        },
+        }
+        Message::Response { .. } => {}
     }
 
     Ok(())
 }
 
-fn handle_http_request(body: &[u8], current_conversation: &mut CurrentConversation) -> anyhow::Result<()> {
+fn handle_http_request(
+    body: &[u8],
+    current_conversation: &mut CurrentConversation,
+) -> anyhow::Result<()> {
     let http_request = http::HttpServerRequest::from_bytes(body)?
         .request()
         .ok_or_else(|| anyhow::anyhow!("Failed to parse http request"))?;
@@ -54,9 +65,10 @@ fn handle_http_request(body: &[u8], current_conversation: &mut CurrentConversati
 
 fn prompt(bytes: &[u8], current_conversation: &mut CurrentConversation) -> anyhow::Result<()> {
     let prompt = serde_json::from_slice::<Prompt>(bytes)?;
-    current_conversation.messages.push(prompt.prompt);
     let answer = get_groq_answer(&prompt.prompt)?;
-    current_conversation.messages.push(answer);
+
+    update_conversation(&prompt.prompt, &answer, current_conversation)?;
+
     http::send_response(
         http::StatusCode::OK,
         Some(HashMap::from([(
@@ -68,26 +80,37 @@ fn prompt(bytes: &[u8], current_conversation: &mut CurrentConversation) -> anyho
     Ok(())
 }
 
+fn update_conversation(prompt: &str, answer: &str, current_conversation: &mut CurrentConversation) -> anyhow::Result<()> {
+    current_conversation.messages.push(prompt.to_string());
+    current_conversation.messages.push(answer.to_string());
+
+    if current_conversation.is_new() {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as i64;
+
+        current_conversation.date_created = Some(now);
+
+        let summary_prompt = format!("Given the following conversation: {:?}, summarize the topic in 80 words or less. Only output the title, do not explain yourself.", current_conversation.messages);
+        let summary_answer = get_groq_answer(&summary_prompt)?;
+        current_conversation.title = Some(summary_answer);
+    }
+
+    Ok(())
+}
+
 call_init!(init);
 fn init(our: Address) {
     println!("begin");
-    if let Err(e) = http::serve_index_html(
-        &our,
-        "ui",
-        false,
-        true,
-        vec![
-            "/",
-            "/prompt",
-        ],
-    ) {
+    if let Err(e) = http::serve_index_html(&our, "ui", false, true, vec!["/", "/prompt"]) {
         panic!("Error binding https paths: {:?}", e);
     }
 
-    /// Note that every even number is going to be a question, and every odd number is going to be an answer
     let mut current_conversation = CurrentConversation {
         title: None,
         messages: vec![],
+        date_created: None,
     };
 
     loop {
--- crackllama/src/structs.rs
+++ crackllama/src/structs.rs
@@ -10,4 +10,18 @@ pub struct CurrentConversation {
     pub title: Option<String>,
     /// Note that every even number is going to be a question, and every odd number is going to be an answer
     pub messages: Vec<String>,
+    pub date_created: Option<i64>,
+}
+
+impl CurrentConversation {
+    /// When a conversation is new, one exchange has been made. That's when we set the date and title
+    pub fn is_new(&self) -> bool {
+        self.messages.len() == 2 && self.date_created.is_none() && self.title.is_none()
+    }
+
+    pub fn clear(&mut self) {
+        self.title = None;
+        self.messages = vec![];
+        self.date_created = None;
+    }
 }
