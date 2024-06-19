6fa37656756e3725a90dbfa4aa3138ce5730d242
jaxs-ribs <luc.baracat@googlemail.com>
2024-06-19 11:44:55 +0200

    React

---
+++ pkg/ui/crackllama-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SpeedReader from './components/SpeedReader';
+
+function App() {
+  return (
+    <div className="App">
+      <SpeedReader />
+    </div>
+  );
+}
+
+export default App;
